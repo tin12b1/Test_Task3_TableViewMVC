95f9c5c764788e3fdf86b81ce7ac64b21c5be69f
Tin Tran <tin12b1@gmail.com>
2017-07-05 14:05:59 +0700

    Initial Commit

---
+++ Test_Task2_TableViewMVC_ToDoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Task2_TableViewMVC_ToDoList
+//
+//  Created by cntt17 on 7/5/17.
+//  Copyright © 2017 cntt17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
